818dc9efaa8927cb14c7bd3f5fc07692ee18dd0a
MrDrProfK <38258014+MrDrProfK@users.noreply.github.com>
2019-03-21 00:43:51 -0400

    -Implemented fundamental parameter validation and parsing for GET requests. -Corrected some minor issues with AJAX client that prevented testing the API when it returns an HTTP status code indicating an error. -Assorted cleanup of unnecessary code/comments.

--- db-connect.php
+++ db-connect.php
@@ -1,15 +1,7 @@
 <?php
-// 	$_DB_CONFIG = parse_ini_file('../../conf/crud_api_db_config.ini');
-// 	echo $_DB_CONFIG['user'];
-// 	echo '<br>';
-// 	echo $_DB_CONFIG['pw'];
-// 	echo '<br>';
-// 	echo $_DB_CONFIG['dbname'];
-// 	echo '<br>';
-// 	echo $_SERVER['SERVER_NAME'];
-// 	echo '<br>';
 
-function openDBConnection(){
+function openDBConnection() {
+
 	try {
 		$_DB_CONFIG = parse_ini_file('../../conf/crud_api_db_config.ini');
 		// establish DB connection
@@ -17,19 +9,10 @@ function openDBConnection(){
     				$_DB_CONFIG['user'], $_DB_CONFIG['pw']);
     				
 		return $dbh;
-// 		$result = $dbh->query("SELECT * from products WHERE name = 'jack'");
-// 		if($result){
-// 			$_arrayOfRows = $result->fetchAll();
-// 			foreach ($_arrayOfRows as $row) {
-// 				print_r($row);
-// 				echo $row['name'];
-// 			}
-// 			echo json_encode($_arrayOfRows);
-// 		}
-		// disconnect from DB
-// 		$dbh = null;
+		
 	} catch (PDOException $e) {
-		print "Error!: " . $e->getMessage() . "<br>";
+		echo "Error!: " . $e->getMessage() . "<br>";
 		exit();
 	}
+	
 }
--- read.php
+++ read.php
@@ -1,8 +1,93 @@
 <?php
 
+require_once 'db-connect.php';
+// readEntries();
 function readEntries() {
-	// extract id param from url
-	$id = explode('?', explode('/', $_SERVER['REQUEST_URI'])[3])[0];
-	echo json_encode(array('id' => $id, 'name' => $_GET['name'],
-			'priceLow' => $_GET['priceLow'], 'priceHigh' => $_GET['priceHigh']));
+
+	$_arr = array();
+	$_urlSplitByForwardSlash = explode('/', $_SERVER['REQUEST_URI']);
+	if (sizeOf($_urlSplitByForwardSlash) > 3) {
+		// extract id param from url
+		$idParam = explode('?', $_urlSplitByForwardSlash[3])[0];
+		$idParam = trim($idParam);
+		if ($idParam != '') {
+			$_ids = explode(',', $idParam);
+		
+			try {
+				foreach ($_ids as $id) {
+					validateID($id);
+				}		
+				$_arr['ids'] = $_ids;
+			} catch (Exception $e) {
+				http_response_code(400);
+				echo 'Caught exception: '. $e->getMessage();
+				return;
+			}
+		}
+	}
+	
+	if(isset($_GET['name'])){
+		$_names = explode(',', $_GET['name']);
+		try {
+			foreach ($_names as $name) {
+				validateName('name', $name);
+			}
+			$_arr['names'] = $_names;
+		} catch (Exception $e) {
+			http_response_code(400);
+			echo 'Caught exception: '. $e->getMessage();
+			return;
+		}
+	}
+	
+	if (isset($_GET['price-low'])) {
+		try {
+			$_arr['price-low'] = validatePriceBound('price-low', $_GET['price-low']);
+		} catch (Exception $e) {
+			http_response_code(400);
+			echo 'Caught exception: '. $e->getMessage();
+			return;
+		}
+	}
+	
+	if (isset($_GET['price-high'])) {
+		try {
+			$_arr['price-high'] = validatePriceBound('price-high', $_GET['price-high']);
+		} catch (Exception $e) {
+			http_response_code(400);
+			echo 'Caught exception: '. $e->getMessage();
+			return;
+		}
+	}
+
+	http_response_code(200);
+}
+
+function validateID($id) {
+
+	$refinedString = htmlspecialchars(trim($id));
+	
+	if (filter_var($refinedString, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1))))
+		return $refinedString;
+		
+	throw new Exception("Invalid value for 'id'. An 'id' must be specified by an int greater than 0.");
+}
+
+function validateName($value) {
+	if (trim($value) === '')
+		throw new Exception("Missing value for parameter 'name'.");
+	
+	$refinedString = htmlspecialchars(trim($value));
+	
+	return $refinedString;
+}
+
+function validatePriceBound($name, $value) {
+
+	$refinedString = htmlspecialchars(trim($value));
+	
+	if (preg_match("/^[0-9]+(\.[0-9]{1,2})?$/", $refinedString))
+		return $refinedString;
+	
+	throw new Exception("Invalid value for '{$name}'.");
 }
